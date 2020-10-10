32df6bf872e7ea95d78d53e868a7ad9a7218238a
Paulo Guilherme Sousa Silva <paulogui@PG.local>
2020-10-10 12:41:19 -0300

    Menu e administrativo do painel portfolio 📊

---
+++ src/admin/AdminMenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AdminMenu = props => {
+    return (
+        <div>
+            <div class="list-group">
+                <a href="#" class="list-group-item list-group-item-action active">Selecione uma opção</a>
+                <a href="#" class="list-group-item list-group-item-action">Portfolio</a>
+            </div>
+        </div>
+    )
+}
+
+export default AdminMenu
---
+++ src/admin/AdminPortfolio.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+
+
+class AdminPortfolio extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <h2>Área administrativa Portfolio</h2>
+        )
+    }
+}
+
+export default AdminPortfolio
