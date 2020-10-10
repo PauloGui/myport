0967b94754b7df532385d2694f2a309955027483
Paulo Guilherme Sousa Silva <paulogui@PG.local>
2020-10-10 12:44:26 -0300

    Instalação da dependencia do db e integração de rotas no painel administrivo 🗺

--- src/admin/Admin.jsx
+++ src/admin/Admin.jsx
@@ -1,13 +1,21 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+
+import AdminMenu from './AdminMenu'
+import AdminPotfolio from './AdminPortfolio'
 
 class Admin extends Component {
     constructor(props) {
         super(props)
     }
 
-    render(){
-        return(
-            <div></div>
+    render() {
+        return (
+            <div>
+                <h2>Painel Administrativo</h2>
+                <Route path={`/`} component={AdminMenu} />
+                <Route path={`${this.props.match.url}/portfolio`} component={AdminPotfolio} />
+            </div>
         )
     }
 }
